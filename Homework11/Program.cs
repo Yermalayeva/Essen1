3e950a9f8d4a1c2591d72eaf3d744dd98a20b509
Natalya Yermolaeva <natali.ermol73@mail.ru>
2022-12-05 22:24:49 +0300

    add new file Homework 11

---
+++ Homework11/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// Например: 456 -> 4; 782-> 8; 918 -> 1
+
+Random random = new Random();
+int ranNumber = random.Next(100, 1000);
+
+Console.WriteLine(ranNumber);
+
+int firstDigit = ranNumber / 100;
+int secondDigit = ranNumber / 10;
+int resultDigit =secondDigit - firstDigit * 10;
+
+Console.WriteLine(resultDigit);
