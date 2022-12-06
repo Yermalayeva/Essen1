098c6d3362ce75cb5787cdfc97dc479884f33565
Natalya Yermolaeva <natali.ermol73@mail.ru>
2022-12-06 17:58:38 +0300

    add new file Homework 13

---
+++ Homework13/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// Например: 645 -> 5; 78 -> 78 -> третьей цифры нет; 32679 -> 6
+
+Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine());
+int [] M = new int [50];
+int k = 0;
+
+while(N > 0)
+{
+    int X = N / 10;
+    int O = N - X * 10;
+    N = X;
+    M[k] = O;
+    k++;
+}
+if(k > 2)
+{
+    Console.WriteLine(M[k - 3]);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
