202044d21636f338e4b9d9c1454b51fb53b3e2f0
Natalya Yermolaeva <natali.ermol73@mail.ru>
2022-12-06 23:29:10 +0300

    add new file Lesson 3

---
+++ Lesson3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите номер четверти: ");
+int number = int.Parse(Console.ReadLine());
+string result = GetDiapazon(number);
+Console.WriteLine(result);
+
+string GetDiapazon (int number)
+{
+    if(number == 1)
+    {
+        return ("x > 0 && y > 0");
+    }
+
+    if(number == 2)
+    {
+        return ("x > 0 && y < 0");
+    }
+
+    if(number == 3)
+    {
+        return ("x < 0 && y < 0");
+    }
+
+    if(number == 4)
+    {
+        return ("x < 0 && y > 0");
+    }
+
+    return ("-1");
+}
