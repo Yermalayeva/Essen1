4be149cb59d8e32b7c587bf1bdcb2e4445c8eacc
Natalya Yermolaeva <natali.ermol73@mail.ru>
2022-12-07 18:46:37 +0300

    add new file Lesson3_2

---
+++ Lesson3_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N/
+// Например: 5 -> 1, 4, 9, 16, 25.;  2 -> 1,4.
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+int [] arr = GetCalculateTheSquaresOfnumbers(num);
+
+PrintArray(arr);
+
+int [] GetCalculateTheSquaresOfnumbers( int N)
+{
+    int [] array = new int[N];
+    for(int i = 0; i < N; i++)
+    {
+        array[i] = (i + 1) * (i + 1);
+    }
+    return array;
+}
+
+void PrintArray(int [] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
